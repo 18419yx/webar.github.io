fb6a664f4ae61605ceffc67768719188ba7ce56f
18419yx <100352365+18419yx@users.noreply.github.com>
2022-06-06 17:22:43 +0800

    Delete WebXR directory

--- WebXR/js/event.js
+++
@@ -1,9 +0,0 @@
-		   AFRAME.registerComponent('event',{
-		   			   init:function(){
-		   				var el = this.el
-						   el.addEventListener("click",function(){
-							   window.open("../planet/mars.html")
-							   window.close()
-						   })
-		   			   }
-		   })
